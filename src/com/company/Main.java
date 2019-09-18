24bb814ba9056ec2f7834872b3681a4fa4e1736a
Dominik Steinmann <dominik.steinmann@students.fhnw.ch>
2019-09-18 17:01:34 +0200

    test equals methode in main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Matrix m1 , m2, m3 , m4;
+        int rows = 3, columns = 4;
+        m1 = new Matrix(rows,columns);
+        m2 = new Matrix(columns,rows);
+        m3 = new Matrix(rows,columns,3);
+        m4 = new Matrix(rows,columns,3);
+
+        System.out.println(m1.equals(m2));
+        System.out.println(m3.equals(m4));
+        m1.multiply(m2);
     }
 }
